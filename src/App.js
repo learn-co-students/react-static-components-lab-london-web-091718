033fa87ca8b465a1242096ef00b9af183569f3a3
Irum <irum@Irums-MacBook-Air.local>
2018-11-19 10:30:18 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import { FordQuoteComponent } from './FordQuoteComponent'
-import MouseComponent from '../MouseComponent'
+import FordQuoteComponent from './FordQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
   render() {
@@ -17,3 +17,5 @@ class App extends Component {
 }
 
 export default App;
+
+
